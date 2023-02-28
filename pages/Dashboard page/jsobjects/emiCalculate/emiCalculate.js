5cf9e3fad84e720ff34366ec3b6ff357a6683cad
Gokul Venkatesh <gokulvenkat678@gmail.com>
2023-02-28 09:05:16 +0000

    emi calculation is finished on  new loan and update page

--- pages/Dashboard page/jsobjects/emiCalculate/emiCalculate.js
+++ pages/Dashboard page/jsobjects/emiCalculate/emiCalculate.js
@@ -1,7 +1,7 @@
 export default {
 			calEmi : () =>{
-				let emi = (loandetailsForm.data.financeAmount * (loandetailsForm.data.financeRate / 100) ) / 12;
-				emi = ((loandetailsForm.data.financeAmount + (emi * loandetailsForm.data.Input3))/loandetailsForm.data.Input3);
+				let emi = (Form1Copy2Copy.data.financeAmountCopy * (Form1Copy2Copy.data.financeRateCopy / 100) ) / 12;
+				emi = ((Form1Copy2Copy.data.financeAmountCopy + (emi * Form1Copy2Copy.data.Input3Copy2Copy))/Form1Copy2Copy.data.Input3Copy2Copy);
 				return Math.round(emi);
 
 			// const emiAmount = loandetailsForm.data.financeAmount * rate *  (1 + rate) * loandetailsForm.data.Input3 / ((1 + rate) * loandetailsForm.data.Input3 - 1);
